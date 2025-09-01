794f12e0a14e422094fa3a521716a5379427a7f2
danirso <danielsrossi43@gmail.com>
2025-08-31 22:55:50 -0300

    a

--- provision-proxy.sh
+++ provision-proxy.sh
@@ -5,13 +5,13 @@ apt-get update
 apt-get install -y nginx
 
 echo ">>> Configurando o Nginx usando o arquivo local..."
-# Remove a configuração padrão
+
 rm /etc/nginx/sites-enabled/default
 
-# Copia o arquivo de configuração no lugar certo
+
 cp /vagrant_nginx/lanchonete.conf /etc/nginx/sites-available/lanchonete.conf
 
-# Ativa a nova configuração
+
 ln -s /etc/nginx/sites-available/lanchonete.conf /etc/nginx/sites-enabled/
 
 echo ">>> Reiniciando o Nginx..."
